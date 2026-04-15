ae7cd80865619b3909d49ae07618aa9d931deecc
Mezea11 <christiancastellanosmeza@gmail.com>
2026-04-15 14:48:18 +0200

    test: expand match engine unit coverage

--- Testing/UnitTests/WheelOfSpeed.UnitTests/MatchEngineTests.cs
+++ Testing/UnitTests/WheelOfSpeed.UnitTests/MatchEngineTests.cs
@@ -49,6 +49,71 @@ public class MatchEngineTests
         updated.RoundResolved.Should().BeTrue();
     }
 
+    [Fact]
+    public void AddPlayer_ShouldThrow_WhenNameAlreadyExists()
+    {
+        var match = _engine.CreateMatch("Alice");
+
+        var act = () => _engine.AddPlayer(match, "alice");
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*already exists*");
+    }
+
+    [Fact]
+    public void StartNextRound_ShouldThrow_WhenNotAllPlayersReadyOnFirstRound()
+    {
+        var match = _engine.CreateMatch("A");
+        _engine.AddPlayer(match, "B");
+        _engine.MarkReady(match, match.Players[0].PlayerId);
+
+        var act = () => _engine.StartNextRound(match, "socket");
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*must be ready*");
+    }
+
+    [Fact]
+    public void ApplySpin_ShouldThrow_WhenPlayerIsNotActive()
+    {
+        var match = BuildReadyMatch();
+        _engine.StartNextRound(match, "network");
+        var nonActivePlayer = match.Players.Single(player => player.PlayerId != match.ActivePlayerId);
+
+        var act = () => _engine.ApplySpin(match, nonActivePlayer.PlayerId, 100);
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*turn*");
+    }
+
+    [Fact]
+    public void ApplyGuess_ShouldThrow_WhenWheelHasNotBeenSpun()
+    {
+        var match = BuildReadyMatch();
+        _engine.StartNextRound(match, "galaxy");
+
+        var act = () => _engine.ApplyGuess(match, match.ActivePlayerId!, "galaxy");
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*spin the wheel*");
+    }
+
+    [Fact]
+    public void RotateTurn_ShouldSwitchPlayerAndResetTurnState()
+    {
+        var match = BuildReadyMatch();
+        _engine.StartNextRound(match, "thunder");
+        var firstActive = match.ActivePlayerId;
+        _engine.ApplySpin(match, firstActive!, 500);
+        match.SecondsLeft = 0;
+
+        _engine.RotateTurn(match);
+
+        match.ActivePlayerId.Should().NotBe(firstActive);
+        match.CurrentWheelValue.Should().BeNull();
+        match.SecondsLeft.Should().Be(45);
+    }
+
     private MatchState BuildReadyMatch()
     {
         var match = _engine.CreateMatch("A");
